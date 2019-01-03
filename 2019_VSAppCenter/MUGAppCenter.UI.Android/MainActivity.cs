b76009ebd1bbc6e9b3315421f472d0811fa7c049
Thomas Kälin <thomas.kaelin@noser.com>
2019-01-03 08:51:42 +0100

    App Center: added new Xamarin.Forms-starter.

---
+++ 2019_VSAppCenter/MUGAppCenter.UI.Android/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using MUGAppCenter.Shared;
+
+namespace MUGAppCenter.UI.Droid
+{
+    [Activity(Label = "MUGAppCenter", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            LoadApplication(new App());
+        }
+    }
+}

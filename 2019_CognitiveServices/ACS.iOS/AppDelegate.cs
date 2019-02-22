7b9b9b22ff52f8e434353aa93b5b0c18cbf1cb50
Thomas Kälin <thomas.kaelin@noser.com>
2019-02-22 11:50:42 +0100

    ACS: added empty Xamarin.Forms-template.

---
+++ 2019_CognitiveServices/ACS.iOS/AppDelegate.cs
@@ -0,0 +1,19 @@
+﻿using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+namespace ACS.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication uiApplication, NSDictionary launchOptions)
+        {
+            Forms.Init();
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(uiApplication, launchOptions);
+        }
+    }
+}
